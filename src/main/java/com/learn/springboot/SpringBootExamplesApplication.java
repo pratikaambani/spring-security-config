29412cd6e5053d81392dd33069433c3d481a19a9
Pratik Ambani <pratikaambani@gmail.com>
2017-01-05 10:25:24 +0530

    Bulk Commit

---
+++ src/main/java/com/learn/springboot/SpringBootExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.learn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExamplesApplication.class, args);
+	}
+}
